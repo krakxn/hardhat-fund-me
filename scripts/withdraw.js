2116c78fef46e57fb8ddb75354d31f28067fb3c2
AN <108299225+krakxn@users.noreply.github.com>
2022-07-02 18:40:22 +0530

    update

--- scripts/withdraw.js
+++ scripts/withdraw.js
@@ -1,12 +1,13 @@
 const { ethers, getNamedAccounts } = require("hardhat")
 
+// Sample script for withdrawing
 async function main() {
     const { deployer } = await getNamedAccounts()
     const fundMe = await ethers.getContract("FundMe", deployer)
     console.log(`Got contract FundMe at ${fundMe.address}`)
     console.log("Withdrawing from contract...")
     const transactionResponse = await fundMe.withdraw()
-    await transactionResponse.wait()
+    await transactionResponse.wait(1) // Waits 1 block
     console.log("Got it back!")
 }
 
