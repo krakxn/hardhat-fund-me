c8036c059f9905e50fb031b0fff63c7f2de335ed
AN <108299225+krakxn@users.noreply.github.com>
2022-07-02 18:39:55 +0530

    updated fund.js

--- scripts/fund.js
+++ scripts/fund.js
@@ -1,5 +1,6 @@
 const { ethers, getNamedAccounts } = require("hardhat")
 
+// Sample script for funding
 async function main() {
     const { deployer } = await getNamedAccounts()
     const fundMe = await ethers.getContract("FundMe", deployer)
@@ -8,7 +9,7 @@ async function main() {
     const transactionResponse = await fundMe.fund({
         value: ethers.utils.parseEther("0.1"),
     })
-    await transactionResponse.wait()
+    await transactionResponse.wait(1)
     console.log("Funded!")
 }
 
