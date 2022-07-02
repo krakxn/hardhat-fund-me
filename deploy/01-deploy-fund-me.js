5f82dea820506c765ef62d5d309a9cb51b6dcbef
AN <108299225+krakxn@users.noreply.github.com>
2022-07-02 18:39:04 +0530

    updated

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -7,22 +7,22 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
     let ethUsdPriceFeedAddress
-    // local network
+    // Local network
     if (chainId == 31337) {
         const ethUsdAggregator = await deployments.get("MockV3Aggregator")
         ethUsdPriceFeedAddress = ethUsdAggregator.address
     } else {
         ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
     }
-    // when going for a local network such as hardhat or localhost we should use a mock
+    // When going for a local network such as hardhat or localhost we should use a mock
     const args = [ethUsdPriceFeedAddress]
     const fundMe = await deploy("FundMe", {
         from: deployer,
-        args: args, // put price feed addy
+        args: args, // Pricefeed address
         log: true,
-        waitConfirmations: network.config.blockConfirmations || 1, // if no blockConfirmations given in hardhat.config
+        waitConfirmations: network.config.blockConfirmations || 1, // If no blockConfirmations given in hardhat.config
     })
-    if (
+    if ( // If not a local network, we verify the contract
         !developmentChains.includes(network.name) &&
         process.env.ETHERSCAN_API_KEY
     ) {
