73e652bb1b6826b6b647b1a54b793e99fc5fe68c
AN <108299225+krakxn@users.noreply.github.com>
2022-07-02 18:36:08 +0530

    update

--- contracts/PriceConverter.sol
+++ contracts/PriceConverter.sol
@@ -1,12 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol"; // Datafeed
 
-// Why is this a library and not abstract?
-// Why not an interface?
+// Library for reuse
 library PriceConverter {
-    // We could make this public, but then we'd have to deploy it
     function getPrice(AggregatorV3Interface priceFeed)
         internal
         view
@@ -15,18 +13,17 @@ library PriceConverter {
         // Rinkeby ETH / USD Address
         // https://docs.chain.link/docs/ethereum-addresses/
         (, int256 answer, , , ) = priceFeed.latestRoundData();
-        // ETH/USD rate in 18 digit
+        // ETH/USD rate in 18 digits
         return uint256(answer * 10000000000); // 10 digits
     }
 
-    // 1000000000
     function getConversionRate(
         uint256 ethAmount,
         AggregatorV3Interface priceFeed
     ) internal view returns (uint256) {
         uint256 ethPrice = getPrice(priceFeed);
         uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000; // 18 digits
-        // the actual ETH/USD conversion rate, after adjusting the extra 0s.
+        // The actual ETH/USD conversion rate, after adjusting the extra 0s.
         return ethAmountInUsd;
     }
 }
