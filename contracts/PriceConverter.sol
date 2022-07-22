0fdf56c4113d315caa46fc7f81b76fb9463a9a3c
AN <108299225+krakxn@users.noreply.github.com>
2022-07-22 19:38:28 +0530

    Update PriceConverter.sol

--- contracts/PriceConverter.sol
+++ contracts/PriceConverter.sol
@@ -1,20 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.8;
 
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol"; // Datafeed
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol"; /// Price datafeed
 
-// Library for reuse
+/// Library for reuse
 library PriceConverter {
     function getPrice(AggregatorV3Interface priceFeed)
         internal
         view
-        returns (uint256)
-    {
-        // Rinkeby ETH / USD Address
-        // https://docs.chain.link/docs/ethereum-addresses/
+        returns (uint256) {
+        /// Rinkeby ETH / USD Address is used
+        /// For a list of addresses: https://docs.chain.link/docs/ethereum-addresses/
         (, int256 answer, , , ) = priceFeed.latestRoundData();
-        // ETH/USD rate in 18 digits
-        return uint256(answer * 10000000000); // 10 digits
+        
+        /// To get ETH/USD rate in 18 digits (Answer is 8 zeros)
+        return uint256(answer * 10000000000); /// 8 zeros * 10 zeros = 18 zeros
     }
 
     function getConversionRate(
@@ -22,8 +22,9 @@ library PriceConverter {
         AggregatorV3Interface priceFeed
     ) internal view returns (uint256) {
         uint256 ethPrice = getPrice(priceFeed);
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000; // 18 digits
-        // The actual ETH/USD conversion rate, after adjusting the extra 0s.
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000; /// 18 digits
+        
+        /// The actual ETH/USD conversion rate, after adjusting the extra 0s.
         return ethAmountInUsd;
     }
 }
