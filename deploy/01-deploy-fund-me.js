b057c5b825fb4d41a26e1e59d577de5e1b47ea28
AN <108299225+krakxn@users.noreply.github.com>
2022-07-22 19:40:55 +0530

    Update 01-deploy-fund-me.js

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,20 +1,22 @@
 const { networkConfig, developmentChains } = require("../helper-hardhat-config")
 const { network, deployments, getNamedAccounts } = require("hardhat")
 const { verify } = require("../utils/verify")
-// Instead of const { getNamedAccounts, deployments } = hre
-module.exports = async ({ getNamedAccounts, deployments }) => {
+
+module.exports = async ({ getNamedAccounts, deployments }) => { // Simplified: const { getNamedAccounts, deployments } = hre
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const chainId = network.config.chainId
     let ethUsdPriceFeedAddress
-    // Local network
+    
+    // Checks for local network
     if (chainId == 31337) {
         const ethUsdAggregator = await deployments.get("MockV3Aggregator")
         ethUsdPriceFeedAddress = ethUsdAggregator.address
     } else {
         ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
     }
-    // When going for a local network such as hardhat or localhost we should use a mock
+    
+    // When going for a local network such as hardhat or localhost, we should use a mock
     const args = [ethUsdPriceFeedAddress]
     const fundMe = await deploy("FundMe", {
         from: deployer,
@@ -22,6 +24,7 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
         log: true,
         waitConfirmations: network.config.blockConfirmations || 1, // If no blockConfirmations given in hardhat.config
     })
+    
     if ( // If not a local network, we verify the contract
         !developmentChains.includes(network.name) &&
         process.env.ETHERSCAN_API_KEY
